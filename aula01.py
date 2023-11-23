5d11e851615d6b3ec218e68a0ae148b8eb36c3bd
BrunoStoelbenn <brunocassiano.stoelbenn@gmail.com>
2023-11-22 23:20:08 -0300

    Pulei para a seção de sql porque já estava ansioso

--- aula01.py
+++ aula01.py
@@ -1,16 +1,34 @@
-"""
-DocString
-E escrever o que eu 
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
 
+cap = cv.VideoCapture(0)  # cv.VideoCapture("video.avi")
 
-quiser
-"""
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
-'''
-Usar para escrever suas notas
-'''
+while True:
+    # Capture frame
+    ret, frame = cap.read()
 
-# Permite escrever um comentário
-print(123) # Na frente
-# Abaixo
-print(456)
+    # Se o frame for lido corretamente, ret é True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Convertemos o quadro para escala de cinza
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Aplicamos o operador de detecção de bordas (Canny, neste caso)
+    edges = cv.Canny(gray, 50, 150)
+
+    # Exibimos o resultado
+    cv.imshow('Detecção de Bordas', edges)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+
+# Quando tudo estiver feito, liberamos a captura
+cap.release()
+cv.destroyAllWindows()
