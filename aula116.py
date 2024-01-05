94d0c1f43d4b54aa24b0ec5b262005c846f86f75
BrunoStoelbenn <brunocassiano.stoelbenn@gmail.com>
2024-01-05 10:40:31 -0300

    Modos de abertura de arquivo e encoding com with open, os.remove, os.unlink e os.rename - apagando, renomeando e movendo arquivos

--- aula116.py
+++ aula116.py
@@ -1,3 +1,4 @@
+import os
 # Criando arquivos com Python + Context Manager with
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
@@ -28,27 +29,41 @@ caminho_arquivo = 'aula116.txt'
 #     print('Olá mundo')
 #     print('Arquivo vai ser fechado\n')
 
-with open(caminho_arquivo, 'w+') as arquivo:
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     print(type(arquivo))
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+
+#     print('READLINES')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' * 10)
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+with open(caminho_arquivo, 'a+') as arquivo:
+    ...
+    print(type(arquivo))
     arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
     arquivo.writelines(
         ('Linha 3\n', 'Linha 4\n')
     )
-    arquivo.seek(0, 0)
-    print(arquivo.read())
-    print('Lendo')
-    arquivo.seek(0, 0)
-    print(arquivo.readline(), end='')
-    print(arquivo.readline().strip())
-    print(arquivo.readline().strip())
 
-    print('READLINES')
-    arquivo.seek(0, 0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
+#os.remove(caminho_arquivo) # ou unlink
+#os.rename(caminho_arquivo, 'aula116-2.txt')
 
-
-print('#' * 10)
-
-with open(caminho_arquivo, 'r') as arquivo:
-    print(arquivo.read())
