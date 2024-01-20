2df255de42b83b0df5f8e58b6f216cbcbbd37c75
BrunoStoelbenn <brunocassiano.stoelbenn@gmail.com>
2024-01-20 14:43:08 -0300

    Continuing the course

---
+++ aula124.py
@@ -0,0 +1,47 @@
+# Mantendo estados dentro da classe
+class Camera:
+    def __init__(self, nome, filmando=False):
+        self.nome = nome
+        self.filmando = filmando
+
+    def filmar(self):
+        if self.filmando:
+            print(f'{self.nome} JÁ está filmando...')
+            return
+
+        print(f'{self.nome} está filmando...')
+        self.filmando = True
+
+    def parar_filmar(self):
+        if not self.filmando:
+            print(f'{self.nome} NÃO está filmando...')
+            return
+
+        print(f'{self.nome} está parando de filmar...')
+        self.filmando = False
+
+    def fotografar(self):
+        if self.filmando:
+            print(f'{self.nome} não pode fotografar filmando')
+            return
+
+        print(f'{self.nome} está fotografando...')
+
+
+c1 = Camera('Canon')
+s2 = Camera('Sony')
+
+c1.filmar()
+c1.filmar()
+c1.fotografar()
+c1.parar_filmar()
+c1.fotografar()
+
+print()
+
+s2.parar_filmar()
+s2.filmar()
+s2.filmar()
+s2.fotografar()
+s2.parar_filmar()
+s2.fotografar()
