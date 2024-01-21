54c7227e839e3b2eb6e89138a40cfe08a55fd097
BrunoStoelbenn <brunocassiano.stoelbenn@gmail.com>
2024-01-20 22:44:10 -0300

    Continuing the course

--- aula126.py
+++ aula126.py
@@ -13,8 +13,10 @@ class Pessoa:
         return Pessoa.ano_atual - self.idade
 
 # Criando uma instância da classe Pessoa
-dados = {'nome': 'João', 'idade': 35}
-p1 = Pessoa(**dados)
+dados_1 = {'nome': 'João', 'idade': 35}
+dados_2 = {'nome' : 'Bruno', 'idade' : 21}
+p1 = Pessoa(**dados_1)
+p2 = Pessoa(**dados_2)
 
 # p1.nome = 'EITA'
 # print(p1.idade)
@@ -29,16 +31,35 @@ p1 = Pessoa(**dados)
 # print(p1.nome)
 
 # Salvando os dados da instância em um arquivo JSON
-with open('pessoa.json', 'w', encoding='utf8') as arquivo_json:
-    json.dump(vars(p1), arquivo_json)
+
+with open('pessoa1.json', 'w', encoding='utf8') as arquivo:
+    json.dump(p1.__dict__, arquivo)
+    # json.dump(vars(p1), arquivo)
 
 # Lendo os dados do arquivo JSON
-with open('pessoa.json', 'r', encoding='utf8') as arquivo_json:
-    dados_salvos = json.load(arquivo_json)
+    
+with open('pessoa1.json', 'r', encoding='utf8') as arquivo:
+    dados_salvos_1 = json.load(arquivo)
+
+# Salvando os dados da instância em um arquivo JSON
+
+with open('pessoa2.json', 'w', encoding='utf8') as arquivo:
+    json.dump(vars(p2), arquivo)
+    # json.dump(vars(p1), arquivo)
+
+# Lendo os dados do arquivo JSON
+    
+with open('pessoa2.json', 'r', encoding='utf8') as arquivo:
+    dados_salvos_2 = json.load(arquivo)
 
 # Criando uma nova instância da classe com os dados salvos
-p2 = Pessoa(**dados_salvos)
+p1_novo = Pessoa(**dados_salvos_1)
+p2_novo = Pessoa(**dados_salvos_2)
 
 # Verificando os atributos da nova instância
-print(vars(p2))
-print(p2.nome)
+
+print(vars(p1_novo))
+print(p1_novo.nome)
+print()
+print(p2_novo.__dict__)
+print(p2_novo.nome)
