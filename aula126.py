23b765b1375787d361c9fb98c99379f2e04d52c9
BrunoStoelbenn <brunocassiano.stoelbenn@gmail.com>
2024-02-29 10:40:29 -0300

    Class methods + factories. i have created some files to keep the upcoming code of the course

--- aula126.py
+++ aula126.py
@@ -9,57 +9,57 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
 
-    def get_ano_nascimento(self):
-        return Pessoa.ano_atual - self.idade
+#     def get_ano_nascimento(self):
+#         return Pessoa.ano_atual - self.idade
 
-# Criando uma instância da classe Pessoa
-dados_1 = {'nome': 'João', 'idade': 35}
-dados_2 = {'nome' : 'Bruno', 'idade' : 21}
-p1 = Pessoa(**dados_1)
-p2 = Pessoa(**dados_2)
+# # Criando uma instância da classe Pessoa
+# dados_1 = {'nome': 'João', 'idade': 35}
+# dados_2 = {'nome' : 'Bruno', 'idade' : 21}
+# p1 = Pessoa(**dados_1)
+# p2 = Pessoa(**dados_2)
 
-# p1.nome = 'EITA'
-# print(p1.idade)
-# p1.__dict__['outra'] = 'coisa'
-# p1.__dict__['nome'] = 'EITA'
-# del p1.__dict__['nome']
-# print(p1.__dict__)
-# print(vars(p1))
-# print(p1.outra)
-# print(p1.nome)
-# print(vars(p1))
-# print(p1.nome)
+# # p1.nome = 'EITA'
+# # print(p1.idade)
+# # p1.__dict__['outra'] = 'coisa'
+# # p1.__dict__['nome'] = 'EITA'
+# # del p1.__dict__['nome']
+# # print(p1.__dict__)
+# # print(vars(p1))
+# # print(p1.outra)
+# # print(p1.nome)
+# # print(vars(p1))
+# # print(p1.nome)
 
-# Salvando os dados da instância em um arquivo JSON
+# # Salvando os dados da instância em um arquivo JSON
 
-with open('pessoa1.json', 'w', encoding='utf8') as arquivo:
-    json.dump(p1.__dict__, arquivo)
-    # json.dump(vars(p1), arquivo)
+# with open('pessoa1.json', 'w', encoding='utf8') as arquivo:
+#     json.dump(p1.__dict__, arquivo)
+#     # json.dump(vars(p1), arquivo)
 
-# Lendo os dados do arquivo JSON
+# # Lendo os dados do arquivo JSON
     
-with open('pessoa1.json', 'r', encoding='utf8') as arquivo:
-    dados_salvos_1 = json.load(arquivo)
+# with open('pessoa1.json', 'r', encoding='utf8') as arquivo:
+#     dados_salvos_1 = json.load(arquivo)
 
-# Salvando os dados da instância em um arquivo JSON
+# # Salvando os dados da instância em um arquivo JSON
 
-with open('pessoa2.json', 'w', encoding='utf8') as arquivo:
-    json.dump(vars(p2), arquivo)
-    # json.dump(vars(p1), arquivo)
+# with open('pessoa2.json', 'w', encoding='utf8') as arquivo:
+#     json.dump(vars(p2), arquivo)
+#     # json.dump(vars(p1), arquivo)
 
-# Lendo os dados do arquivo JSON
+# # Lendo os dados do arquivo JSON
     
-with open('pessoa2.json', 'r', encoding='utf8') as arquivo:
-    dados_salvos_2 = json.load(arquivo)
+# with open('pessoa2.json', 'r', encoding='utf8') as arquivo:
+#     dados_salvos_2 = json.load(arquivo)
 
-# Criando uma nova instância da classe com os dados salvos
-p1_novo = Pessoa(**dados_salvos_1)
-p2_novo = Pessoa(**dados_salvos_2)
+# # Criando uma nova instância da classe com os dados salvos
+# p1_novo = Pessoa(**dados_salvos_1)
+# p2_novo = Pessoa(**dados_salvos_2)
 
-# Verificando os atributos da nova instância
+# # Verificando os atributos da nova instância
 
-print(vars(p1_novo))
-print(p1_novo.nome)
-print()
-print(p2_novo.__dict__)
-print(p2_novo.nome)
+# print(vars(p1_novo))
+# print(p1_novo.nome)
+# print()
+# print(p2_novo.__dict__)
+# print(p2_novo.nome)
--- teste.py
+++ teste.py
@@ -1,1 +1,10 @@
-print(2330 % 11)
+n = int(input(""))  # Digitar o número de valores que será lido
+vetor = [int(x) for x in input().split()]  # Digitar os valores que serão armazenados dentro do vetor fornecendo sempre um espaço entre eles
+
+menor_valor, posicao = vetor[0], 0  # Criação das variáveis menor_valor que já possui o vetor[0] dentro dela e posicao que já possui o 0 dentro dela
+for n in range(1, n):  # Laço de repetição que começa no primeiro indíce e vai ser lido até o índice n fornecido na primeira linha do código
+    if vetor[n] < menor_valor:  # Compara se o vetor da posição [n] é menor do que o menor_valor
+        menor_valor, posicao = vetor[n], n  # Se o vetor da posição [n] for menor do que o menor_valor, então o menor_valor assume o vetor[n] e a posição fica o próprio n
+
+print("Menor valor:", menor_valor)  #  Impressão do menor valor
+print("Posicao:", posicao)  # Impressão da posição
